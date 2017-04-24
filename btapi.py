e44c0f0d5f5718d65f9d3af3c80cd0d22a47d062
Elliot Penn <epenn28@vt.edu>
2017-04-24 16:57:32 -0400

    Get next bus code implemented

---
+++ btapi.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import requests
+import xml.etree.ElementTree as ET
+
+TORG = "1114"
+BURRUSS = "1101"
+SQUIRESWEST = "1113"
+SQUIRESEAST = "1110"
+NEWMAN = "1100"
+
+
+def getNextBus(routeName, stopCode):
+    userData = {'routeShortName': routeName, 'stopCode': stopCode}
+    r = requests.post("http://216.252.195.248/webservices/bt4u_webservice.asmx/GetNextDepartures", data=userData)
+    root = ET.fromstring(r.text)
+    try:
+        nextBus = root[0].find('AdjustedDepartureTime').text
+        (nextDate, nextTime) = nextBus.split(" ", 1)
+        print("The next bus for {} stop {} will show up at {}.".format(routeName, stopCode, nextTime))
+        return (routeName, stopCode, nextTime)
+    except IndexError:
+        print("You did something wrong, dummy")
+    
+    
+def main():
+    (routeName, stopCode, nextTime) = getNextBus("HWDB", TORG)
+    print("Function returned", routeName, stopCode, nextTime)
+    
+    
+if __name__ == '__main__':
+    main()
