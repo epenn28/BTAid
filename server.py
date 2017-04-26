7ae05361728613b44b19a360e81640b2fb4b9932
Elliot Penn <epenn28@vt.edu>
2017-04-25 20:37:40 -0400

    Hey this kinda works

--- server.py
+++ server.py
@@ -63,16 +63,16 @@ def main():
 
     messages = [{"beaconid": "B9407F30-F5F8-466E-AFF9-25556B57FE6D", 
     	"pid": "caml323", "route": "UCB", 
-    	"phone": "+12404860906", "name": "Cory Latham"}, 
+    	"phone": "+12404995406", "name": "Cory Latham"}, 
     	{"beaconid": "D0B32A8C-B407-AD88-D6DB-5E88C25E3438", 
     	"pid": "epenn28", "route": "HWDB", 
     	"phone": "+12404995406", "name": "Elliot Penn"},
     	{"beaconid": "EA8FCA33-C569-5E09-3260-E0D038256D3B", 
     	"pid": "kosovo", "route": "HWDA", 
-    	"phone": "+15409981120", "name": "Daulet Talapkaliyev"},
+    	"phone": "+12404995406", "name": "Daulet Talapkaliyev"},
     	{"beaconid": "394CD435-D71C-DCD9-1C8D-1218CE4DFE62", 
     	"pid": "anajahd4", "route": "UMS", 
-    	"phone": "+17577086232", "name": "Anajah Delestre"}]
+    	"phone": "+12404995406", "name": "Anajah Delestre"}]
 
     collection.insert_many(messages)
     
@@ -83,15 +83,15 @@ def main():
     
 def on_request(ch, method, props, body):
     global output
-    global collection
+    #global collection
 
     request_msg = body
 
     print(" [.] BLE beacon UUID received:%s" % request_msg)
 
-    (beaconUUID, busStop) = make_tuple(request_msg.decode())
+    (busStop, beaconUUID) = make_tuple(request_msg.decode())
     temp = collection.find_one({"beaconid": beaconUUID})
-    
+    print(beaconUUID)
     phoneNumber = temp['phone']
     busRoute = temp['route']
     
