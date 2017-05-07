430cecd4fd8b2e3c87b0aaafc79df6d1a0ec798a
Elliot Penn <epenn28@vt.edu>
2017-05-07 19:42:41 -0400

    Added test output

--- beacontest.py
+++ beacontest.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 #uuid="B9407F30-F5F8-466E-AFF9-25556B57FE6D"
+#,device_filter=IBeaconFilter(uuid="8492e75f-4fd6-469d-b132-043fe94921d8")
 import time
 from beacontools import BeaconScanner, IBeaconFilter
 
@@ -19,9 +20,14 @@ def callback(bt_addr, rssi, packet, additional_info):
         avg = total / 15
         total = 0
         print("Average:", avg)
-    print("MAC address:", bt_addr)
-    print("RSSI:", rssi)
+    #print("MAC address:", bt_addr)
+    #print("RSSI:", rssi)
+    print("UUID:", additional_info['uuid'])
     #print("<%s, %d> %s %s" % (bt_addr, rssi, packet, additional_info))
+    """This returns the following:
+    <fb:91:ba:46:32:06, -54> IBeaconAdvertisement<tx_power: -58, uuid: b9407f30-f5f8-466e-aff9-25556b57fe6d, major: 43043, minor: 7457> 
+    /{'major': 43043, 'uuid': 'b9407f30-f5f8-466e-aff9-25556b57fe6d', 'minor': 7457}
+    """
 
 # scan for all iBeacon advertisements from beacons with the specified uuid
 scanner = BeaconScanner(callback)
